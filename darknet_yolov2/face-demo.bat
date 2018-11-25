064b2dd0bb6992c2c3ce48150e802b2eef7a43f4
abars <abarsceo@gmail.com>
2018-11-25 21:09:09 +0900

    Add yolov2 sample script

---
+++ darknet_yolov2/face-demo.bat
@@ -0,0 +1,1 @@
+darknet.exe detector demo data/face.data cfg/yolov2-tiny.cfg backup-face/yolov2-tiny-train_413600.weights -c 0
---
+++ darknet_yolov2/face-train.bat
@@ -0,0 +1,1 @@
+darknet.exe detector train data/face.data cfg/yolov2-tiny-train.cfg yolov2-tiny.weights
