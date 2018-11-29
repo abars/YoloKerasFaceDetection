f3729840ee7794b9c58856201d73183f758f56fd
abars <abarsceo@gmail.com>
2018-11-29 10:42:26 +0900

    Add support utkface

--- annotation_utkface_keras.py
+++ annotation_utkface_keras.py
@@ -1,5 +1,5 @@
 #Generate annotation for keras
-#https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/
+#https://susanqq.github.io/UTKFace/
 
 from scipy import io as spio
 from datetime import datetime
@@ -7,15 +7,16 @@ from datetime import datetime
 import numpy as np
 import os
 import shutil
+import glob
 
-if(os.path.exists("./dataset/imdb_crop/")):
+if(os.path.exists("./dataset/UTKFace/")):
 	DATASET_ROOT_PATH=""
 else:
 	DATASET_ROOT_PATH="/Volumes/TB4/Keras/"
 
 OUTPUT_LABEL="agegender"
 
-IMDB_PATH=DATASET_ROOT_PATH+"dataset/imdb_crop/"
+UTKFACE_PATH=DATASET_ROOT_PATH+"dataset/UTKFace/"
 GENDER_PATH=DATASET_ROOT_PATH+"dataset/"+OUTPUT_LABEL+"/annotations/gender/"
 AGE_PATH=DATASET_ROOT_PATH+"dataset/"+OUTPUT_LABEL+"/annotations/age/"
 AGE101_PATH=DATASET_ROOT_PATH+"dataset/"+OUTPUT_LABEL+"/annotations/age101/"
@@ -63,26 +64,8 @@ if(not os.path.exists(AGE101_PATH)):
 		os.mkdir(AGE101_PATH+"train/"+format(i, '03d'))
 		os.mkdir(AGE101_PATH+"validation/"+format(i, '03d'))
 
-def calc_age(taken, dob):
-	birth = datetime.fromordinal(max(int(dob) - 366, 1))
-	if birth.month < 7:
-		return taken - birth.year
-	else:
-		return taken - birth.year - 1
-
-def is_valid(face_score,second_face_score,age,gender):
-	if face_score < 1.0:
-		return False
-	if (~np.isnan(second_face_score)) and second_face_score > 0.0:
-		return False
-	if ~(0 <= age <= 100):
-		return False
-	if np.isnan(gender):
-		return False
-	return True
-
 def get_gender_path(gender):
-	if(gender==0.0):
+	if(gender==1):
 		return "f"
 	return "m"
 
@@ -103,27 +86,21 @@ def get_age_path(age):
 		return "48-53"
 	return "60-"
 
-meta = spio.loadmat(IMDB_PATH+"imdb.mat")
+i=0
+for image_path in glob.glob(UTKFACE_PATH+"*.jpg"):
+	image_name = os.path.basename(image_path)
+	print image_name
+	age, gender = image_name.split("_")[:2]
+	gender=int(gender)
+	age=min(int(age), 100)
 
-db = "imdb"
-
-full_path = meta[db][0, 0]["full_path"][0]
-dob = meta[db][0, 0]["dob"][0]
-gender = meta[db][0, 0]["gender"][0]
-photo_taken = meta[db][0, 0]["photo_taken"][0]
-face_score = meta[db][0, 0]["face_score"][0]
-second_face_score = meta[db][0, 0]["second_face_score"][0]
-age = [calc_age(photo_taken[i], dob[i]) for i in range(len(dob))]
-
-for i in range(len(full_path)):
-	if(not is_valid(face_score[i],second_face_score[i],age[i],gender[i])):
-		continue
-	print "path:"+str(full_path[i])+" gender:"+str(gender[i])
-	print ""+get_age_path(age[i])+" "+get_gender_path(gender[i])
 	train_or_validation="train"
 	if(i%4==0):
 		train_or_validation="validation"
-	src_img=IMDB_PATH+full_path[i][0]
-	shutil.copyfile(src_img, AGE_PATH+train_or_validation+"/"+get_age_path(age[i])+"/"+str(i)+".jpg")
-	shutil.copyfile(src_img, GENDER_PATH+train_or_validation+"/"+get_gender_path(gender[i])+"/"+str(i)+".jpg")
-	shutil.copyfile(src_img, AGE101_PATH+train_or_validation+"/"+format(age[i],"03d")+"/"+str(i)+".jpg")
+
+	src_img=UTKFACE_PATH+image_name
+	shutil.copyfile(src_img, AGE_PATH+train_or_validation+"/"+get_age_path(age)+"/"+str(i)+".jpg")
+	shutil.copyfile(src_img, GENDER_PATH+train_or_validation+"/"+get_gender_path(gender)+"/"+str(i)+".jpg")
+	shutil.copyfile(src_img, AGE101_PATH+train_or_validation+"/"+format(age,"03d")+"/"+str(i)+".jpg")
+
+	i=i+1
