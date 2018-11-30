f19ef3ff09de8681fa74d3e981ce5cc68b441083
abars <abarsceo@gmail.com>
2018-11-30 22:20:29 +0900

    Remove 80 class training script

---
+++ darknet_yolov2/face-demo-one-class.bat
@@ -0,0 +1,1 @@
+darknet.exe detector demo data/face-one-class.data cfg/yolov2-tiny-train-one-class.cfg c:/deeplearning/backup_face_one_class/yolov2-tiny-train-one-class_32600.weights -c 0
--- darknet_yolov2/face-demo.bat
+++
@@ -1,1 +0,0 @@
-darknet.exe detector demo data/face.data cfg/yolov2-tiny-train.cfg c:/deeplearning/backup_face/yolov2-tiny-train_431800.weights -c 0
