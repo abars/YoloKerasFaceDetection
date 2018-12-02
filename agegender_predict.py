6fbb129082f68af2b8ccf5b1be1db423502e05a4
abars <abarsceo@gmail.com>
2018-12-02 09:58:48 +0900

    Test some images

--- agegender_predict.py
+++ agegender_predict.py
@@ -17,19 +17,11 @@ from keras.models import load_model
 # MODE
 # ----------------------------------------------
 
-#ANNOTATIONS='agegender'
-ANNOTATIONS='gender'
-#ANNOTATION='age'
-#ANNOTATION='emotion'
-
-#MODELS="vgg16"
-#MODELS="small_cnn"
-#MODELS="simple_cnn"
-#MODELS="miniXception"
-MODELS="squeezenet"
-#MODELS="mobilenet"
-
+ANNOTATIONS=''
+MODELS=""
 DATASET_ROOT_PATH=""
+DATA_AUGUMENTATION=False
+
 #DATASET_ROOT_PATH="/Volumes/TB4/Keras/"
 
 # ----------------------------------------------
@@ -42,14 +34,14 @@ if len(sys.argv) >= 3:
   if len(sys.argv) >= 4:
     DATASET_ROOT_PATH=sys.argv[3]
 else:
-  print("usage: python agegender_predict.py [agegender/gender/age/age101/emotion] [inceptionv3/vgg16/small_cnn/simple_cnn/miniXception/squeezenet/octavio] [datasetroot(optional)]")
+  print("usage: python agegender_predict.py [agegender/gender/age/age101/emotion] [inceptionv3/vgg16/squeezenet/octavio] [datasetroot(optional)]")
   sys.exit(1)
 
 if ANNOTATIONS!="agegender" and ANNOTATIONS!="gender" and ANNOTATIONS!="age" and ANNOTATIONS!="age101" and ANNOTATIONS!="emotion":
   print("unknown annotation mode");
   sys.exit(1)
 
-if MODELS!="inceptionv3" and MODELS!="vgg16" and MODELS!="small_cnn" and MODELS!="simple_cnn" and MODELS!="miniXception" and MODELS!="squeezenet" and MODELS!="mobilenet" and MODELS!="octavio":
+if MODELS!="inceptionv3" and MODELS!="vgg16" and MODELS!="squeezenet" and MODELS!="mobilenet" and MODELS!="octavio":
   print("unknown network mode");
   sys.exit(1)
 
@@ -57,16 +49,21 @@ if MODELS!="inceptionv3" and MODELS!="vgg16" and MODELS!="small_cnn" and MODELS!
 # converting
 # ----------------------------------------------
 
-MODEL_HDF5=DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.hdf5'
+AUGUMENT=""
+if(DATA_AUGUMENTATION):
+  AUGUMENT="augumented"
+
+MODEL_HDF5=DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+AUGUMENT+'.hdf5'
 ANNOTATION_WORDS='words/agegender_'+ANNOTATIONS+'_words.txt'
 
 if(ANNOTATIONS=="emotion"):
-	MODEL_HDF5=DATASET_ROOT_PATH+'pretrain/fer2013_mini_XCEPTION.102-0.66.hdf5'
 	ANNOTATION_WORDS='words/emotion_words.txt'
 
 if(MODELS=="octavio"):
-	MODEL_HDF5=DATASET_ROOT_PATH+'pretrain/gender_mini_XCEPTION.21-0.95.hdf5'
-	ANNOTATION_WORDS='words/agegender_gender_words.txt'
+	if(ANNOTATIONS=="emotion"):
+		MODEL_HDF5=DATASET_ROOT_PATH+'pretrain/fer2013_mini_XCEPTION.102-0.66.hdf5'
+	if(ANNOTATIONS=="gender"):
+		MODEL_HDF5=DATASET_ROOT_PATH+'pretrain/gender_mini_XCEPTION.21-0.95.hdf5'
 
 if(MODELS=="mobilenet"):
 	import keras
@@ -77,21 +74,18 @@ else:
 	keras_model = load_model(MODEL_HDF5)
 keras_model.summary()
 
-shape = keras_model.layers[0].get_output_at(0).get_shape().as_list()
-IMAGE_SIZE = shape[1]
-
 # ----------------------------------------------
 # test
 # ----------------------------------------------
 
 image_list=[
-'dataset/agegender/annotations/agegender/validation/0_0-2_m/landmark_aligned_face.84.8277643357_43f107482d_o.jpg',
-'dataset/agegender/annotations/agegender/validation/11_15-20_f/landmark_aligned_face.290.11594063605_713764ddeb_o.jpg',
-'dataset/agegender/annotations/agegender/validation/3_15-20_m/landmark_aligned_face.291.11593667615_2cb80d1c2a_o.jpg',
-'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/f/26707.jpg',
-'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/f/26761.jpg',
-'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/m/181.jpg',
-'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/m/83.jpg'
+	'dataset/agegender/annotations/agegender/validation/0_0-2_m/landmark_aligned_face.84.8277643357_43f107482d_o.jpg',
+	'dataset/agegender/annotations/agegender/validation/11_15-20_f/landmark_aligned_face.290.11594063605_713764ddeb_o.jpg',
+	'dataset/agegender/annotations/agegender/validation/3_15-20_m/landmark_aligned_face.291.11593667615_2cb80d1c2a_o.jpg',
+	'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/f/26707.jpg',
+	'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/f/26761.jpg',
+	'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/m/181.jpg',
+	'/Volumes/TB4/Keras/dataset/agegender/annotations/gender/train/m/83.jpg'
 ]
 
 for image in image_list:
@@ -102,7 +96,8 @@ for image in image_list:
 	print image
 	img = cv2.imread(image)
 
-	img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+	shape = keras_model.layers[0].get_output_at(0).get_shape().as_list()
+	img = cv2.resize(img, (shape[1], shape[2]))
 	if(ANNOTATIONS=='emotion' or MODELS=='octavio'):
 		img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		img = np.expand_dims(img, axis=2)
@@ -134,20 +129,14 @@ for image in image_list:
 # convert to caffe model
 # ----------------------------------------------
 
-sys.exit(1)
-
-import keras2caffe
-import caffe
-
-keras2caffe.convert(keras_model, DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.prototxt', DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.caffemodel')
-net  = caffe.Net(DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.prototxt', DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.caffemodel', caffe.TEST)
-
-data = data.transpose((0, 3, 1, 2))
-
-out = net.forward_all(data = data)
-pred = out[net.outputs[0]]
-
-prob = np.max(pred)
-cls = pred.argmax()
-print prob, cls, lines[cls]
+#import keras2caffe
+#import caffe
+#keras2caffe.convert(keras_model, DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.prototxt', DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.caffemodel')
+#net  = caffe.Net(DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.prototxt', DATASET_ROOT_PATH+'pretrain/agegender_'+ANNOTATIONS+'_'+MODELS+'.caffemodel', caffe.TEST)
+#data = data.transpose((0, 3, 1, 2))
+#out = net.forward_all(data = data)
+#pred = out[net.outputs[0]]
+#prob = np.max(pred)
+#cls = pred.argmax()
+#print prob, cls, lines[cls]
 
