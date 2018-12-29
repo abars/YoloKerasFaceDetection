12915a47a063fa00056cb2a5e8adbada60ff2a21
abars <abarsceo@gmail.com>
2018-12-29 20:41:36 +0900

    Support python3 for download_model, Move pretrained model location to github release

--- download_model.py
+++ download_model.py
@@ -2,20 +2,38 @@
 
 import os
 import sys
-import urllib2
+if sys.version_info >= (3,0):
+	from urllib import request
+else:
+	import urllib2
 
 def main(argv):
 	OUTPUT_PATH="./pretrain/"
 	if not os.path.isdir(OUTPUT_PATH):
 		os.mkdir(OUTPUT_PATH)
+	print("1/3");
 	with open(OUTPUT_PATH+'agegender_age101_squeezenet.hdf5','wb') as f:
-		f.write(urllib2.urlopen("http://www.abars.biz/keras/agegender_age101_squeezenet.hdf5").read())
+		path="https://github.com/abars/YoloKerasFaceDetection/releases/download/1.0.0/agegender_age101_squeezenet.hdf5"
+		if sys.version_info >= (3,0):
+			f.write(request.urlopen(path).read())
+		else:
+			f.write(urllib2.urlopen(path).read())
 		f.close()
+	print("2/3");
 	with open(OUTPUT_PATH+'agegender_gender_squeezenet.hdf5','wb') as f:
-		f.write(urllib2.urlopen("http://www.abars.biz/keras/agegender_gender_squeezenet.hdf5").read())
+		path="https://github.com/abars/YoloKerasFaceDetection/releases/download/1.0.0/agegender_gender_squeezenet.hdf5"
+		if sys.version_info >= (3,0):
+			f.write(request.urlopen(path).read())
+		else:
+			f.write(urllib2.urlopen(path).read())
 		f.close()
+	print("3/3");
 	with open(OUTPUT_PATH+'yolov2_tiny-face.h5','wb') as f:
-		f.write(urllib2.urlopen("http://www.abars.biz/keras/yolov2_tiny-face.h5").read())
+		path="https://github.com/abars/YoloKerasFaceDetection/releases/download/1.0.0/yolov2_tiny-face.h5"
+		if sys.version_info >= (3,0):
+			f.write(request.urlopen(path).read())
+		else:
+			f.write(urllib2.urlopen(path).read())
 		f.close()
 
 if __name__=='__main__':
