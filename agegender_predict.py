f544c3ecae7f0930285d762d50ddc00ad7337bf6
abars <abarsceo@gmail.com>
2019-01-06 19:46:00 +0900

    Add benchmark mode

--- agegender_predict.py
+++ agegender_predict.py
@@ -12,6 +12,9 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+
+import matplotlib.pyplot as plt
 
 # ----------------------------------------------
 # MODE
@@ -21,7 +24,7 @@ ANNOTATIONS=''
 DATASET_NAME=''
 MODELS=""
 DATASET_ROOT_PATH=""
-CONVERT_TO_CAFFEMODEL=False
+OPTIONAL_MODE=""
 DATA_AUGUMENTATION=False
 
 #DATASET_ROOT_PATH="/Volumes/TB4/Keras/"
@@ -38,9 +41,9 @@ if len(sys.argv) >= 3:
   if len(sys.argv) >= 5:
     DATASET_ROOT_PATH=sys.argv[4]
   if len(sys.argv) >= 6:
-    CONVERT_TO_CAFFEMODEL=sys.argv[5]
+    OPTIONAL_MODE=sys.argv[5]
 else:
-  print("usage: python agegender_predict.py [gender/age/age101/emotion] [inceptionv3/vgg16/squeezenet/octavio] [adience/imdb/utk/appareal/vggface2/empty] [datasetroot(optional)] [caffemodel(optional)]")
+  print("usage: python agegender_predict.py [gender/age/age101/emotion] [inceptionv3/vgg16/squeezenet/octavio] [adience/imdb/utk/appareal/vggface2/empty] [datasetroot(optional)] [benchmark/caffemodel(optional)]")
   sys.exit(1)
 
 if ANNOTATIONS!="gender" and ANNOTATIONS!="age" and ANNOTATIONS!="age101" and ANNOTATIONS!="emotion":
@@ -55,19 +58,14 @@ if DATASET_NAME!="adience" and DATASET_NAME!="imdb" and DATASET_NAME!="utk" and 
   print("unknown dataset name");
   sys.exit(1)
 
-if CONVERT_TO_CAFFEMODEL!=False and CONVERT_TO_CAFFEMODEL!="caffemodel":
-  print("unknown caffemodel mode");
+if OPTIONAL_MODE!="" and OPTIONAL_MODE!="benchmark" and OPTIONAL_MODE!="caffemodel":
+  print("unknown optional mode");
   sys.exit(1)
 
 if DATASET_NAME=="empty":
 	DATASET_NAME=""
 else:
 	DATASET_NAME='_'+DATASET_NAME
-
-if CONVERT_TO_CAFFEMODEL=="caffemodel":
-	CONVERT_TO_CAFFEMODEL=True
-else:
-	CONVERT_TO_CAFFEMODEL=False
 
 # ----------------------------------------------
 # converting
@@ -102,7 +100,7 @@ keras_model.summary()
 # convert to caffe model
 # ----------------------------------------------
 
-if CONVERT_TO_CAFFEMODEL:
+if OPTIONAL_MODE=="caffemodel":
 	os.environ["GLOG_minloglevel"] = "2"
 	import caffe
 	import keras2caffe
@@ -111,7 +109,88 @@ if CONVERT_TO_CAFFEMODEL:
 	keras2caffe.convert(keras_model, prototxt, caffemodel)
 
 # ----------------------------------------------
-# test
+# Benchmark
+# ----------------------------------------------
+
+if OPTIONAL_MODE=="benchmark":
+	BENCHMARK_DATASET_NAME="imdb"
+	BENCHMARK_DATASET_TARGET="validation"
+	BATCH_SIZE=64
+
+	shape = keras_model.layers[0].get_output_at(0).get_shape().as_list()
+
+	disp_generator = ImageDataGenerator(rescale=1.0 / 255).flow_from_directory(
+	   DATASET_ROOT_PATH+'dataset/agegender_'+BENCHMARK_DATASET_NAME+'/annotations/'+ANNOTATIONS+'/'+BENCHMARK_DATASET_TARGET,
+	   target_size=(shape[1], shape[2]),
+	   batch_size=BATCH_SIZE,
+	   class_mode='categorical'
+	)
+
+	DISTRIBUTION_FILE=DATASET_ROOT_PATH+'pretrain/benchmark_'+ANNOTATIONS+"_"+MODELS+"_"+DATASET_NAME+'.png'
+
+	fig = plt.figure()
+	ax1 = fig.add_axes((0.1, 0.6, 0.8, 0.3))
+	ax2 = fig.add_axes((0.1, 0.1, 0.8, 0.3))
+	ax1.tick_params(labelbottom="on")
+	ax2.tick_params(labelleft="on")
+
+	max_cnt=len(disp_generator.filenames)
+	#max_cnt=10
+
+	x=np.zeros((max_cnt))
+	y=np.zeros((max_cnt))
+	t=np.zeros((max_cnt))
+
+	for key in disp_generator.class_indices.keys():
+		print(""+str(disp_generator.class_indices[key])+"/"+key)
+
+	cnt=0
+	heatmap=np.zeros((len(disp_generator.class_indices),len(disp_generator.class_indices)))
+	for x_batch, y_batch in disp_generator:
+		for i in range(BATCH_SIZE):
+			x[cnt]=y_batch[i][0]
+			t[cnt]=y_batch[i].argmax()
+
+			data=x_batch[i]
+			data.shape = (1,) + data.shape
+			pred = keras_model.predict(data)[0]
+			cls = pred.argmax()
+
+			y[cnt]=cls
+
+			heatmap[int(y[cnt]),int(t[cnt])]=heatmap[int(y[cnt]),int(t[cnt])]+1
+
+			cnt=cnt+1
+			print(""+str(cnt)+"/"+str(max_cnt)+" ground truth:"+str(y_batch[i].argmax())+" predicted:"+str(cls))
+			if cnt>=max_cnt:
+				break
+		if cnt>=max_cnt:
+			break
+
+	ax1.pcolor(heatmap, cmap=plt.cm.Blues)
+	for y in range(heatmap.shape[0]):
+		for x in range(heatmap.shape[1]):
+			ax1.text(x + 0.5, y + 0.5, '%.4f' % heatmap[y, x],
+				horizontalalignment='center',
+				verticalalignment='center',
+			)
+
+	ax1.set_title('ground truth vs predicted '+ANNOTATIONS)
+	ax1.set_xlabel(ANNOTATIONS+'(ground truth)')
+	ax1.set_ylabel(ANNOTATIONS+'(predicted)')
+	ax1.legend(loc='upper right')
+
+	ax2.hist(t, bins=len(disp_generator.class_indices))
+	ax2.set_title('distribution of ground truth '+ANNOTATIONS)
+	ax2.set_xlabel(ANNOTATIONS+'(ground truth)')
+	ax2.set_ylabel('count')
+	ax2.legend(loc='upper right')
+	
+	fig.savefig(DISTRIBUTION_FILE)
+	sys.exit(1)
+
+# ----------------------------------------------
+# Normal test
 # ----------------------------------------------
 
 if(os.path.exists("./dataset/agegender_adience/")):
@@ -174,7 +253,7 @@ for image in image_list:
 # Test caffemodel
 # ----------------------------------------------
 
-	if CONVERT_TO_CAFFEMODEL:
+	if OPTIONAL_MODE=="caffemodel":
 		net  = caffe.Net(prototxt, caffemodel, caffe.TEST)
 		data = data.transpose((0, 3, 1, 2))
 		out = net.forward_all(data = data)
